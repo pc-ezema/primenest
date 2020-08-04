d3ff62a8644a878476eeeda692a03caefc19a10d
Martins Abiodun <martinsabiodun94@gmail.com>
2020-08-04 12:25:38 +0100

    change httpsProtocol location

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,8 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        // appending custom middleware 
-        \App\Http\Middleware\HttpsProtocol::class,
         
     ];
 
@@ -41,6 +39,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpsProtocol::class
         ],
 
         'api' => [
