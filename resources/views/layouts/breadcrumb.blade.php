79661f3364a35e7ac858925b7c75ddd401b4e1e3
zeefola <zeefola@gmail.com>
2020-09-26 15:40:34 +0100

    Breadcrumb  markups

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -4,9 +4,14 @@
                 <div class="col-lg-12">
                     <div class="breadcrumb-inner">
                         <h1 class="page-title"> {{ $title }} </h1>
-                        <ul class="page-list">
-                            <li><a href="/">Home</a></li>
-                            <li> {{ $subtitle }} </li>
+                        <ul itemscope itemtype="http://schema.org/BreadcrumbList"  class="page-list">
+                            <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/"> <span itemprop="name"> Home </span> </a>
+                                <meta itemprop="position" content="1" />
+                            </li>
+                            ›
+                            <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"> <a itemprop="item" href="{{ request()->fullUrl() }}"> <span itemprop="name"> {{ $subtitle }} </span> 
+                                <meta itemprop="position" content="2" />
+                            </li>
                         </ul>
                     </div>
                 </div>
