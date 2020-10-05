6ddc1362846fe1a0f113260a357f3197ed56c879
zeefola <zeefola@gmail.com>
2020-10-05 11:42:49 +0100

    Added deleted at to application db

--- app/application_history.php
+++ app/application_history.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class application_history extends Model
 {
-    //
+    use SoftDeletes;
 }
